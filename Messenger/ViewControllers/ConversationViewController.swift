b598da180b44e48d731542de2cb0ce6c6df30b08
Valerian1999 <bossysleeper99@gmail.com>
2020-11-22 16:19:36 +0700

    Loading convesation part 1

--- Messenger/ViewControllers/ConversationViewController.swift
+++ Messenger/ViewControllers/ConversationViewController.swift
@@ -12,10 +12,12 @@ import FirebaseAuth
 
 class ConversationViewController: UIViewController {
     
+    private var conversations = [Conversation]()
+    
     private var tableView : UITableView = {
         let table = UITableView()
         table.isHidden = true
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "CellId")
+        table.register(ConversationTableViewCell.self, forCellReuseIdentifier: ConversationTableViewCell.identifier)
         return table
     }()
     
@@ -38,6 +40,43 @@ class ConversationViewController: UIViewController {
         view.addSubview(noConversationLabel)
         setUpTableView()
         fetchConversation()
+        startListeningForCOnversations()
+    }
+    
+    private func startListeningForCOnversations() {
+        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+            return
+        }
+//
+//        if let observer = loginObserver {
+//            NotificationCenter.default.removeObserver(observer)
+//        }
+//
+//        print("starting conversation fetch...")
+//
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
+        DatabaseManager.shared.getAllConversations(for: safeEmail, completion: { [weak self] result in
+            switch result {
+            case .success(let conversations):
+                print("successfully got conversation models")
+                guard !conversations.isEmpty else {
+                    self?.tableView.isHidden = true
+                    self?.noConversationLabel.isHidden = false
+                    return
+                }
+                self?.noConversationLabel.isHidden = true
+                self?.tableView.isHidden = false
+                self?.conversations = conversations
+
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                self?.tableView.isHidden = true
+                self?.noConversationLabel.isHidden = false
+                print("failed to get convos: \(error)")
+            }
+        })
     }
     
     @objc private func didTapComposeButton() {
@@ -134,21 +173,31 @@ class ConversationViewController: UIViewController {
 extension ConversationViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return conversations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
-        cell.textLabel?.text = "Hello"
-        cell.accessoryType = .disclosureIndicator
+        let model = conversations[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: ConversationTableViewCell.identifier,
+                                                 for: indexPath) as! ConversationTableViewCell
+        cell.configure(with: model)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let vc = ChatViewController(with: "asdasdadasd", id: "dasdasdas")
-        vc.title = "Valerian"
+        let model = conversations[indexPath.row]
+        openConversation(model)
+    }
+    
+    func openConversation(_ model: Conversation) {
+        let vc = ChatViewController(with: model.otherUserEmail, id: model.id)
+        vc.title = model.name
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
     }
 }
