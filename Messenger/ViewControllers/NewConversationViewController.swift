2b4dd3d35afa4e58c8bd0c5a63c6c7eff0106606
Valerian1999 <bossysleeper99@gmail.com>
2020-11-22 12:37:12 +0700

    add all

--- Messenger/ViewControllers/NewConversationViewController.swift
+++ Messenger/ViewControllers/NewConversationViewController.swift
@@ -9,27 +9,15 @@ import UIKit
 
 class NewConversationViewController: UIViewController {
     
+    private var users = [[String: String]]()
+    private var results = [[String: String]]()
+    private var hasFetched = false
+    
     private var searchBar : UISearchBar = {
         let searchbar = UISearchBar()
         searchbar.placeholder = "Search for users....."
         return searchbar
     }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        searchBar.delegate = self
-        navigationController?.navigationBar.topItem?.titleView = searchBar
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel",
-                                                            style: .done,
-                                                            target: self,
-                                                            action: #selector(dismissSelf))
-        searchBar.becomeFirstResponder()
-    }
-    
-    @objc private func dismissSelf() {
-        dismiss(animated: true, completion: nil)
-    }
     
     private var tableView : UITableView = {
         let table = UITableView()
@@ -47,9 +35,116 @@ class NewConversationViewController: UIViewController {
         label.font = .systemFont(ofSize: 21, weight: .medium)
         return label
     }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        view.addSubview(noConversationLabel)
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(dismissSelf))
+        searchBar.becomeFirstResponder()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        noConversationLabel.frame = CGRect(x: view.width/4,
+                                           y: (view.heigh - 200)/2  ,
+                                           width: view.width/2,
+                                           height: 200)
+    }
+    
+    @objc private func dismissSelf() {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
+//MARK: - UITableViewDelegate,UITableViewDataSource
+extension NewConversationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
+        cell.textLabel?.text = results[indexPath.row]["name"]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // start conversation
+    }
+}
+
+//MARK: - UISearchBarDelegate
 extension NewConversationViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+            return
+        }
+        searchBar.resignFirstResponder()
+
+        results.removeAll()
+        searchUsers(query: text)
+    }
+    
+    func searchUsers(query: String) {
+        // check if array has firebase results
+        if hasFetched {
+            // if it does: filter
+            filterUsers(with: query)
+        }
+        else {
+            // if not, fetch then filter
+            DatabaseManager.shared.getAllUsers(completion: { [weak self] result in
+                switch result {
+                case .success(let usersCollection):
+                    self?.hasFetched = true
+                    self?.users = usersCollection
+                    self?.filterUsers(with: query)
+                case .failure(let error):
+                    print("Failed to get usres: \(error)")
+                }
+            })
+        }
+    }
+    
+    func filterUsers(with term: String) {
+        guard hasFetched else {
+            return
+        }
+        
+        let results: [[String : String]] = users.filter({
+//            guard let email = $0["email"], email != safeEmail else {
+//                return false
+//            }
+            guard let name = $0["name"]?.lowercased() else {
+                return false
+            }
+            return name.hasPrefix(term.lowercased())
+        })
+        self.results = results
+        updateUI()
+    }
+    
+    func updateUI() {
+        if results.isEmpty {
+            noConversationLabel.isHidden = false
+            tableView.isHidden = true
+        }
+        else {
+            noConversationLabel.isHidden = true
+            tableView.isHidden = false
+            tableView.reloadData()
+        }
     }
 }
